b974ac02ea05b7aafa7f099d779d9ce501b59e29
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-09 03:46:54 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,14 @@
-def hello(array)
+
+# my_collect
+#   can handle an empty collection
+#   yields the correct element from a given collection, in this case languages
+#   returns a new collection of appropriately modified elements, in this case capitalized languages
+#   does not modify the original collection
+#   yields the correct element from the given collection, in this case students
+#   returns a new collection of appropriately modified elements, in this case student first names
+#   does not modify the original collection
+
+def my_collect(array)
   i = 0
   collection = []
   while i < array.length
